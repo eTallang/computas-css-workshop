7ad21292ea714695abae9f45740af16b27fdd6cd
Erik Tallang <e.tallang@gmail.com>
2021-05-26 19:45:24 +0200

    feat: dark mode

--- src/circle.ts
+++ src/circle.ts
@@ -1,5 +1,8 @@
+import Mouse from "./types";
+
 export default class Circle {
-  private scale: number;
+  private scale = 1;
+  private friction = 0.9;
 
   constructor(
     private context: CanvasRenderingContext2D,
@@ -10,10 +13,11 @@ export default class Circle {
     private radius: number,
     private color: string,
     private canvasHeight: number,
-    private canvasWidth: number
+    private canvasWidth: number,
+    private gravity = 0
   ) {}
 
-  draw(mouseX: number, mouseY: number) {
+  draw(mouse: Mouse) {
     if (this.x - this.radius < 0) {
       this.dx = -this.dx;
     } else if (this.x + this.radius > this.canvasWidth) {
@@ -22,17 +26,24 @@ export default class Circle {
 
     if (this.y - this.radius < 0) {
       this.dy = -this.dy;
-    } else if (this.y + this.radius > this.canvasHeight) {
+    } else if (this.y + this.radius + this.dy > this.canvasHeight) {
       this.dy = -this.dy;
+      if (this.gravity > 0) {
+        this.dy = this.dy * this.friction;
+      }
+    } else {
+      this.dy += this.gravity;
     }
 
     this.x += this.dx;
     this.y += this.dy;
 
-    if (Math.abs(this.x - mouseX) < 100 && Math.abs(this.y - mouseY) < 100) {
-      this.scale = 4;
-    } else {
-      this.scale = 1;
+    if (Math.abs(this.x - mouse.x) < 100 && Math.abs(this.y - mouse.y) < 100) {
+      if (this.scale < 60) {
+        this.scale++;
+      }
+    } else if (this.scale > 0) {
+      this.scale--;
     }
 
     this.createCircle();
@@ -40,7 +51,7 @@ export default class Circle {
 
   private createCircle() {
     this.context.beginPath();
-    this.context.arc(this.x, this.y, this.radius * this.scale, 0, Math.PI * 2);
+    this.context.arc(this.x, this.y, this.radius + this.scale, 0, Math.PI * 2);
     this.context.closePath();
     this.context.fillStyle = this.color;
     this.context.fill();
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import "../styles.scss";
 import Circle from "./circle";
+import Mouse from './types';
 
 const container = document.getElementById("root") as HTMLCanvasElement;
 const context = container.getContext("2d");
+const backgroundColor = 'rgba(16, 18, 25, 1)';
+container.style.backgroundColor = backgroundColor;
 
 let canvasHeight = window.innerHeight;
 let canvasWidth = window.innerWidth;
 
-let mouseX: number;
-let mouseY: number;
+const mouse: Mouse = {
+  x: undefined,
+  y: undefined
+}
 
+// const colors = ["#D94862", "#344973", "#F2E750", "#D98218", "#F26849"];
+const colors = ["#fffce0", '#D94862',"#F26849"];
 let circles: Circle[] = [];
-let maxRadius = 10;
+const speedX = 2;
+const speedY = 1.5;
+let maxRadius = 5;
 
 function generateCircles(amount: number) {
   for (let i = 0; i < amount; i++) {
@@ -19,17 +28,35 @@ function generateCircles(amount: number) {
     const radius = size * maxRadius;
     const x = Math.random() * (canvasWidth - radius * 2) + radius;
     const y = Math.random() * (canvasHeight - radius * 2) + radius;
-    const color = `rgba(255, 99, 71, ${size})`
-    const dy = size;
-    const dx = size;
-
-    circles.push(new Circle(context, x, y, dx, dy, radius, color, canvasHeight, canvasWidth));
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    const dx = Math.random() * size * speedX * (1 - 2 * Math.round(Math.random()));
+    const dy = Math.random() * size * speedY * (1 - 2 * Math.round(Math.random()));
+    circles.push(
+      new Circle(
+        context,
+        x,
+        y,
+        dx,
+        dy,
+        radius,
+        color,
+        canvasHeight,
+        canvasWidth,
+        0
+      )
+    );
   }
 }
 
 function render() {
+  // To add motion blur
+  // context.fillStyle = 'rgba(16, 18, 25, 0.5)';
+  // context.fillRect(0, 0, canvasWidth, canvasHeight);
+  
+  // To clear for each render
   context.clearRect(0, 0, canvasWidth, canvasHeight);
-  circles.forEach(circle => circle.draw(mouseX, mouseY));
+
+  circles.forEach((circle) => circle.draw(mouse));
 
   window.requestAnimationFrame(render);
 }
@@ -39,10 +66,6 @@ function setCanvasSize() {
   container.height = canvasHeight;
 }
 
-setCanvasSize();
-generateCircles(1000);
-render();
-
 window.addEventListener("resize", () => {
   canvasWidth = window.innerWidth;
   canvasHeight = window.innerHeight;
@@ -50,19 +73,16 @@ window.addEventListener("resize", () => {
 });
 
 window.addEventListener("mousemove", (e: MouseEvent) => {
-  mouseX = e.x;
-  mouseY = e.y;
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
 });
 
+window.addEventListener("mouseout", (e: MouseEvent) => {
+  mouse.x = undefined;
+  mouse.y = undefined;
+});
 
-
-// function drawLine(event: MouseEvent) {
-//   context.beginPath();
-
-//   context.arc(event.x, event.y, 60 + Math.random() * 20, 1, 5, true);
-//   context.closePath();
-//   context.fillStyle = "tomato";
-//   context.fill();
-// }
-
-// document.addEventListener("mousedown", drawLine);
+// App initiation
+setCanvasSize();
+generateCircles(400);
+render();
